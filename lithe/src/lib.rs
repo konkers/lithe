511f78ee12945a711f3eb32780d5331cdbc4d8f7
Erik Gilling <konkers@konkers.net>
2020-12-15 20:56:59 -0800

    Move common code to lithe library.
    - Extract pin interpretation and USB device into lithe library.
    - Update samd21-mini to use lithe library.
    - Add buttons to samd21-mini.
    - Fix pin definitions for samd21-mini.

---
+++ lithe/src/lib.rs
@@ -0,0 +1,129 @@
+#![no_std]
+
+use {
+    usb_device::{
+        bus::{UsbBus, UsbBusAllocator},
+        prelude::*,
+    },
+    usbd_hid::descriptor::generator_prelude::*,
+    usbd_hid::hid_class::HIDClass,
+};
+
+#[derive(Debug)]
+pub struct PinState {
+    pub joy_right: bool,
+    pub joy_left: bool,
+    pub joy_up: bool,
+    pub joy_down: bool,
+    pub buttons: [bool; 10],
+}
+
+#[gen_hid_descriptor(
+    (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = JOYSTICK) = {
+        (usage = X,) = {x=input;};
+        (usage = Y,) = {y=input;};
+        (usage_page = BUTTON, usage_min = BUTTON_1, usage_max = BUTTON_8) = {
+            #[packed_bits 8] #[item_settings data,variable,absolute] buttons0=input;
+        };
+        (usage_page = BUTTON, usage_min = 9, usage_max = 10) = {
+            #[packed_bits 2] #[item_settings data,variable,absolute] buttons1=input;
+        };
+    }
+)]
+#[derive(PartialEq)]
+pub struct InputReport {
+    pub x: u8,
+    pub y: u8,
+    pub buttons0: u8,
+    pub buttons1: u8,
+}
+
+pub struct Device<'a, USB: UsbBus> {
+    hid: HIDClass<'a, USB>,
+    dev: UsbDevice<'a, USB>,
+    report: InputReport,
+    report_pending: bool,
+}
+
+impl<'a, USB: UsbBus> Device<'a, USB> {
+    pub fn new(bus: &'a UsbBusAllocator<USB>, serial_num: &'a str) -> Self {
+        let hid = HIDClass::new(bus, InputReport::desc(), 1);
+
+        let dev = UsbDeviceBuilder::new(bus, UsbVidPid(0, 0x3825))
+            .manufacturer("Villa Labs")
+            .product("Lithe Joystick")
+            .serial_number(serial_num)
+            .device_class(0x00)
+            .device_sub_class(0x00)
+            .device_protocol(0x00)
+            .build();
+
+        Self {
+            hid: hid,
+            dev: dev,
+            report: InputReport {
+                x: 0x7f,
+                y: 0x7f,
+                buttons0: 0x0,
+                buttons1: 0x0,
+            },
+            report_pending: false,
+        }
+    }
+
+    pub fn process_pins(&mut self, pins: &PinState) {
+        let prev_report = self.report.clone();
+        self.report.x = if pins.joy_right {
+            0xff
+        } else if pins.joy_left {
+            0x00
+        } else {
+            0x7f
+        };
+
+        self.report.y = if pins.joy_down {
+            0xff
+        } else if pins.joy_up {
+            0x0
+        } else {
+            0x7f
+        };
+
+        self.report.buttons0 = 0;
+        for i in 0..8 {
+            self.report.buttons0 |= if pins.buttons[i] { 1 << i } else { 0x0 };
+            //self.report.buttons0 <<= 1;
+        }
+
+        self.report.buttons1 = 0;
+        for i in 9..=8 {
+            self.report.buttons1 |= if pins.buttons[i] { 0x1 } else { 0x0 };
+            self.report.buttons1 <<= 1;
+        }
+
+        //self.report.buttons0 = if pins.buttons[0] { 0x1 } else { 0x0 };
+
+        if prev_report != self.report {
+            self.report_pending = true;
+        }
+
+        if self.report_pending {
+            self.report_pending = match self.hid.push_input(&self.report) {
+                Err(_) => true,
+                _ => false,
+            };
+        }
+    }
+
+    pub fn usb_poll(&mut self) {
+        self.dev.poll(&mut [&mut self.hid]);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
--- targets/samd21-mini/src/main.rs
+++ targets/samd21-mini/src/main.rs
@@ -4,21 +4,24 @@
 extern crate atsamd_hal as hal;
 
 use {
-    cortex_m::interrupt::free as disable_interrupts,
+    arrayvec::ArrayString,
+    core::fmt::{self, Write},
+    core::str,
     hal::{
         clock::GenericClockController,
         define_pins,
-        gpio::{self, Floating, Input, IntoFunction, Pa0, Pa1, Pa2, Pa3, Port, PullUp},
+        gpio::{
+            self, Floating, Input, IntoFunction, Pa14, Pa15, Pa2, Pa20, Pa21, Pa4, Pa6, Pa7, Pa8,
+            Pa9, Pb8, Pb9, Port, PullUp,
+        },
         prelude::*,
         target_device as pac,
         timer::TimerCounter,
         usb::UsbBus,
     },
+    lithe,
     rtic::app,
-    rtic::{Exclusive, Mutex},
-    usb_device::{bus::UsbBusAllocator, prelude::*},
-    usbd_hid::descriptor::generator_prelude::*,
-    usbd_hid::hid_class::HIDClass,
+    usb_device::bus::UsbBusAllocator,
 };
 
 #[cfg(not(feature = "semihosting"))]
@@ -29,33 +32,22 @@ use {
     cortex_m_semihosting::hprintln,
     panic_semihosting as _, // logs messages to the host stderr; requires a debugger
 };
-#[gen_hid_descriptor(
-    (collection = APPLICATION, usage_page = GENERIC_DESKTOP, usage = JOYSTICK) = {
-        (usage = X,) = {x=input;};
-        (usage = Y,) = {y=input;};
-        (usage_page = BUTTON, usage_min = BUTTON_1, usage_max = BUTTON_8) = {
-            #[packed_bits 8] #[item_settings data,variable,absolute] buttons0=input;
-        };
-        (usage_page = BUTTON, usage_min = 9, usage_max = 10) = {
-            #[packed_bits 2] #[item_settings data,variable,absolute] buttons1=input;
-        };
-    }
-)]
-#[derive(PartialEq)]
-pub struct InputReport {
-    pub x: u8,
-    pub y: u8,
-    pub buttons0: u8,
-    pub buttons1: u8,
-}
 
 define_pins!(
     struct Pins,
     target_device: pac,
-    pin joy_right = a0,
-    pin joy_left = a1,
-    pin joy_up = a2,
-    pin joy_down = a3,
+    pin joy_right = b8,
+    pin joy_left = a2,
+    pin joy_up = a4,
+    pin joy_down = b9,
+    pin button0 = a8,
+    pin button1 = a9,
+    pin button2 = a14,
+    pin button3 = a15,
+    pin button4 = a20,
+    pin button5 = a21,
+    pin button6 = a6,
+    pin button7 = a7,
     pin usb_dm = a24,
     pin usb_dp = a25,
 );
@@ -64,20 +56,26 @@ define_pins!(
 const APP: () = {
     struct Resources {
         usb_bus: &'static UsbBusAllocator<UsbBus>,
-        usb_device: UsbDevice<'static, UsbBus>,
-        usb_hid: HIDClass<'static, UsbBus>,
+        lithe: lithe::Device<'static, UsbBus>,
         timer: TimerCounter<pac::TC4>,
-        joy_right: Pa0<Input<PullUp>>,
-        joy_left: Pa1<Input<PullUp>>,
-        joy_up: Pa2<Input<PullUp>>,
-        joy_down: Pa3<Input<PullUp>>,
-        report: InputReport,
-        report_pending: bool,
+        joy_right: Pb8<Input<PullUp>>,
+        joy_left: Pa2<Input<PullUp>>,
+        joy_up: Pa4<Input<PullUp>>,
+        joy_down: Pb9<Input<PullUp>>,
+        button0: Pa8<Input<PullUp>>,
+        button1: Pa9<Input<PullUp>>,
+        button2: Pa14<Input<PullUp>>,
+        button3: Pa15<Input<PullUp>>,
+        button4: Pa20<Input<PullUp>>,
+        button5: Pa21<Input<PullUp>>,
+        button6: Pa6<Input<PullUp>>,
+        button7: Pa7<Input<PullUp>>,
     }
 
     #[init]
     fn init(ctx: init::Context) -> init::LateResources {
         static mut USB_BUS: Option<UsbBusAllocator<UsbBus>> = None;
+        static mut SERIAL: Option<ArrayString<[u8; 64]>> = None;
 
         // Alias peripherals
         let mut dp: pac::Peripherals = ctx.device;
@@ -95,6 +93,14 @@ const APP: () = {
         let joy_left = pins.joy_left.into_pull_up_input(&mut pins.port);
         let joy_up = pins.joy_up.into_pull_up_input(&mut pins.port);
         let joy_down = pins.joy_down.into_pull_up_input(&mut pins.port);
+        let button0 = pins.button0.into_pull_up_input(&mut pins.port);
+        let button1 = pins.button1.into_pull_up_input(&mut pins.port);
+        let button2 = pins.button2.into_pull_up_input(&mut pins.port);
+        let button3 = pins.button3.into_pull_up_input(&mut pins.port);
+        let button4 = pins.button4.into_pull_up_input(&mut pins.port);
+        let button5 = pins.button5.into_pull_up_input(&mut pins.port);
+        let button6 = pins.button6.into_pull_up_input(&mut pins.port);
+        let button7 = pins.button7.into_pull_up_input(&mut pins.port);
 
         let usb_dm = pins.usb_dm.into_function(&mut pins.port);
         let usb_dp = pins.usb_dp.into_function(&mut pins.port);
@@ -105,23 +111,27 @@ const APP: () = {
             usb_clock, &mut dp.PM, usb_dm, usb_dp, dp.USB,
         )));
 
-        #[cfg(feature = "semihosting")]
-        hprintln!("Initializing peripherals").unwrap();
+        let (serial0, serial1, serial2, serial3) = unsafe {
+            let serial0: *const u32 = 0x0080A00C as *const u32;
+            let serial1: *const u32 = 0x0080A040 as *const u32;
+            let serial2: *const u32 = 0x0080A044 as *const u32;
+            let serial3: *const u32 = 0x0080A048 as *const u32;
+            (*serial0, *serial1, *serial2, *serial3)
+        };
+        *SERIAL = Some(ArrayString::new());
+        write!(
+            SERIAL.as_mut().unwrap(),
+            "{:08X}{:08X}{:08X}{:08X}",
+            serial0,
+            serial1,
+            serial2,
+            serial3
+        )
+        .unwrap();
 
         #[cfg(feature = "semihosting")]
         hprintln!("Preparing HID mouse...").unwrap();
-        let usb_hid = HIDClass::new(USB_BUS.as_ref().unwrap(), InputReport::desc(), 1);
-
-        #[cfg(feature = "semihosting")]
-        hprintln!("Defining USB parameters...").unwrap();
-        let usb_device = UsbDeviceBuilder::new(USB_BUS.as_ref().unwrap(), UsbVidPid(0, 0x3821))
-            .manufacturer("JoshFTW")
-            .product("BBTrackball")
-            .serial_number("RustFW")
-            .device_class(0x00)
-            .device_sub_class(0x00)
-            .device_protocol(0x00)
-            .build();
+        let lithe = lithe::Device::new(USB_BUS.as_ref().unwrap(), SERIAL.as_ref().unwrap());
 
         #[cfg(feature = "semihosting")]
         hprintln!("Setting up timer...").unwrap();
@@ -130,77 +140,109 @@ const APP: () = {
         let tc45 = &clocks.tc4_tc5(&gclk0).unwrap();
         // instantiate a timer objec for the TC4 peripheral
         let mut timer = TimerCounter::tc4_(tc45, dp.TC4, &mut dp.PM);
+        timer.start(2.khz());
         timer.enable_interrupt();
-        timer.start(2.hz());
 
         #[cfg(feature = "semihosting")]
         hprintln!("Defining late resources...").unwrap();
         init::LateResources {
             usb_bus: USB_BUS.as_ref().unwrap(),
-            usb_device,
-            usb_hid,
+            lithe,
             timer,
             joy_right,
             joy_left,
             joy_up,
             joy_down,
-            report: InputReport {
-                x: 0x7f,
-                y: 0x7f,
-                buttons0: 0x0,
-                buttons1: 0x0,
-            },
-            report_pending: false,
+            button0,
+            button1,
+            button2,
+            button3,
+            button4,
+            button5,
+            button6,
+            button7,
         }
     }
 
-    #[task(binds = TC4, resources = [joy_right, joy_left, joy_up, joy_down, report, report_pending, timer, usb_hid])]
+    #[task(binds = TC4, resources = [button0, button1, button2, 
+        button3, button4, button5, button6, button7, joy_right, 
+        joy_left, joy_up, joy_down, lithe, timer])]
     fn tc4_handler(ctx: tc4_handler::Context) {
-        let prev_report = ctx.resources.report.clone();
-        ctx.resources.report.x = if ctx.resources.joy_right.is_low().unwrap() {
-            0xff
-        } else if ctx.resources.joy_left.is_low().unwrap() {
-            0x00
-        } else {
-            0x7f
+        let pins = lithe::PinState {
+            joy_right: ctx.resources.joy_right.is_low().unwrap(),
+            joy_left: ctx.resources.joy_left.is_low().unwrap(),
+            joy_up: ctx.resources.joy_up.is_low().unwrap(),
+            joy_down: ctx.resources.joy_down.is_low().unwrap(),
+            buttons: [
+                ctx.resources.button0.is_low().unwrap(),
+                ctx.resources.button1.is_low().unwrap(),
+                ctx.resources.button2.is_low().unwrap(),
+                ctx.resources.button3.is_low().unwrap(),
+                ctx.resources.button4.is_low().unwrap(),
+                ctx.resources.button5.is_low().unwrap(),
+                ctx.resources.button6.is_low().unwrap(),
+                ctx.resources.button7.is_low().unwrap(),
+                false,
+                false,
+            ],
         };
-
-        ctx.resources.report.y = if ctx.resources.joy_down.is_low().unwrap() {
-            0xff
-        } else if ctx.resources.joy_up.is_low().unwrap() {
-            0x00
-        } else {
-            0x7f
-        };
-
-        if prev_report != *ctx.resources.report {
-            *ctx.resources.report_pending = true;
-        }
-
-        if *ctx.resources.report_pending {
-            *ctx.resources.report_pending =
-                send_report(Exclusive(ctx.resources.usb_hid), &ctx.resources.report);
-        }
-
+        //   rprintln!("t: {:?}", &pins);
+        ctx.resources.lithe.process_pins(&pins);
         ctx.resources.timer.wait().ok();
     }
 
-    #[task(binds = USB, resources = [usb_device, usb_hid])]
+    #[task(binds = USB, resources = [lithe])]
     fn usb_handler(ctx: usb_handler::Context) {
-        let dev = ctx.resources.usb_device;
-        let hid = ctx.resources.usb_hid;
-
-        // USB dev poll only in the interrupt handler
-        dev.poll(&mut [hid]);
+        ctx.resources.lithe.usb_poll();
     }
 };
 
-fn send_report(
-    mut shared_hid: impl Mutex<T = HIDClass<'static, UsbBus>>,
-    report: &InputReport,
-) -> bool {
-    shared_hid.lock(|hid| match hid.push_input(report) {
-        Err(_) => true,
-        _ => false,
-    })
+pub struct ByteMutWriter<'a> {
+    pub buf: &'a mut [u8],
+    pub cursor: usize,
+}
+
+impl<'a> ByteMutWriter<'a> {
+    pub fn new(buf: &'a mut [u8]) -> Self {
+        ByteMutWriter { buf, cursor: 0 }
+    }
+
+    pub fn as_str(&self) -> &str {
+        str::from_utf8(&self.buf[0..self.cursor]).unwrap()
+    }
+
+    #[inline]
+    pub fn capacity(&self) -> usize {
+        self.buf.len()
+    }
+
+    pub fn clear(&mut self) {
+        self.cursor = 0;
+    }
+
+    pub fn len(&self) -> usize {
+        self.cursor
+    }
+
+    pub fn empty(&self) -> bool {
+        self.cursor == 0
+    }
+
+    pub fn full(&self) -> bool {
+        self.capacity() == self.cursor
+    }
+}
+
+impl fmt::Write for ByteMutWriter<'_> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        let cap = self.capacity();
+        for (i, &b) in self.buf[self.cursor..cap]
+            .iter_mut()
+            .zip(s.as_bytes().iter())
+        {
+            *i = b;
+        }
+        self.cursor = usize::min(cap, self.cursor + s.as_bytes().len());
+        Ok(())
+    }
 }
